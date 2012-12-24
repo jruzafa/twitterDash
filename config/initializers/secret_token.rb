c3017620aee9216723fdcba1454cde7a97977a8c
Jose Ruzafa <jose.ruzafa@interactiv4.com>
2012-12-24 11:53:54 +0100

    Skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterPics::Application.config.secret_token = '304502c7a8b3d6b29efadf2ce21e85b904816778bf56d2b93d4ce7e0ba683965269bd2a0db5b5f67efabbe96d7b5f93a0010a2065f07e3417538443de2f19ef6'
