8ef6a35dcbda3d9dd8db20f33a1a9709219a4e34
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-12-24 15:30:17 +0100

    modified README, remove index.html from public folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 TwitterPics::Application.routes.draw do
+
   root :to => 'index#index'
 
   get "index/index"
-
-  get "pics/index"
-
-  get "pics/user"
-
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
