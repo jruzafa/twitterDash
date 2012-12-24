62cd15c1c5c3e6a0cd35f92ea12cfe56d77fab8f
Jose Ruzafa <jose.ruzafa@interactiv4.com>
2012-12-24 13:10:01 +0100

    add file for initialize the data of access for api twitter

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+	config.consumer_key = 'Nz4UKzwu0Z5wUWiWIrzxKA'
+	config.consumer_secret = 'yfNWMC9UDp03dxyUFq7Ujo4UxwAIHTYK4j3tG5faU'
+	config.oauth_token = '22762164-gvdZWpr4e82poePEhjOSE1IeH3mqdm5MtaYENP7QJ'
+	config.oauth_token_secret = 'ztrm4SMiKvu6SgxH07I9naCtrturgykmbIfb8USd9o'
+end
